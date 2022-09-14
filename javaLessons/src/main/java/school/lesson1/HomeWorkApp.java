d880ec9a6404b0cd527fab8328d8a7c3085d144d
NatalyaKhrol <tashapas@yahoo.com>
2022-09-14 20:40:53 +0300

    pom.xml was adjusted

--- javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -41,9 +41,9 @@ public class HomeWorkApp {
         int b = 19;
 
         if (a >= b) {
-            System.out.println("a>=b");
+            System.out.println("a >= b");
         } else {
-            System.out.println("a<b");
+            System.out.println("a < b");
         }
     }
 }
